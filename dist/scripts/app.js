8cc70b5b053746afad0ad892ea58b911f76f4b6d
Ashwin Khurana <ashwin@ashwinkhurana.com>
2016-11-11 07:53:19 -0800

    checkpoint-5-controllers: completed

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -7,6 +7,7 @@
 
         $stateProvider.state('landing', {
             url: '/',
+            controller: 'LandingCtrl as landing',
             templateUrl: '/templates/landing.html'
         })
 
@@ -17,6 +18,7 @@
 
         $stateProvider.state('collection', {
             url: '/collection',
+            controller: 'CollectionCtrl as collection',
             templateUrl: '/templates/collection.html'
         });
     }
